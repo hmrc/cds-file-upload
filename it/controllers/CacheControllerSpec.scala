755488c86c67656b9eb23439bc8e6a7d08ac641e
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2019-03-20 17:16:31 +0000

    CDSID-916 | added authentication for endpoints

--- it/controllers/CacheControllerSpec.scala
+++ it/controllers/CacheControllerSpec.scala
@@ -2,14 +2,20 @@ package controllers
 
 import domain.FileState.Uploaded
 import domain.{BatchFileUpload, File, MRN}
+import org.mockito.ArgumentMatchers._
+import org.mockito.Mockito._
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{BeforeAndAfterEach, MustMatchers, OptionValues, WordSpec}
+import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.Json
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import suite.MongoSuite
+import uk.gov.hmrc.auth.core.AuthConnector
 
+import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class CacheControllerSpec extends WordSpec with MustMatchers
@@ -17,9 +23,17 @@ class CacheControllerSpec extends WordSpec with MustMatchers
   with ScalaFutures
   with IntegrationPatience
   with OptionValues
-  with BeforeAndAfterEach {
+  with BeforeAndAfterEach
+  with MockitoSugar {
 
-  private lazy val builder: GuiceApplicationBuilder = new GuiceApplicationBuilder()
+  val mockAuthConnector = mock[AuthConnector]
+
+  when(mockAuthConnector.authorise[Option[String]](any(), any())(any(), any()))
+    .thenReturn(Future.successful(Some("InternalId")))
+
+  private lazy val builder: GuiceApplicationBuilder =
+    new GuiceApplicationBuilder()
+      .overrides(bind[AuthConnector].toInstance(mockAuthConnector))
 
   override def beforeEach(): Unit = {
     database.map(_.drop()).futureValue
