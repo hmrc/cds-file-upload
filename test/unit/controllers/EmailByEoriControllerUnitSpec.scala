e80a3dfe4ebebfa4971f53a3be86006cd087a2af
Tom Robinson <25112739+magicaljellybeans@users.noreply.github.com>
2024-10-11 16:41:05 +0100

    CEDS-6100 Add new endpoint without eori
    Add new AuthActionWithEori to retrieve eori from auth service

--- test/unit/controllers/EmailByEoriControllerUnitSpec.scala
+++ test/unit/controllers/EmailByEoriControllerUnitSpec.scala
@@ -32,12 +32,12 @@ import scala.concurrent.Future
 class EmailByEoriControllerUnitSpec extends ControllerUnitSpec {
 
   private val connector = mock[CustomsDataStoreConnector]
-  private val controller = new EmailByEoriController(authAction, connector, stubControllerComponents())
+  private val controller = new EmailByEoriController(authActionWithEori, connector, stubControllerComponents())
 
   override protected def beforeEach(): Unit = {
     super.beforeEach()
     reset(connector)
-    authorisedUser()
+    authorisedUserWithEori()
   }
 
   override protected def afterEach(): Unit = {
@@ -77,6 +77,42 @@ class EmailByEoriControllerUnitSpec extends ControllerUnitSpec {
       when(connector.getEmailAddress(any[String])(any[HeaderCarrier])).thenReturn(Future.successful(None))
 
       val response = controller.getEmailIfVerified(TestData.eori)(getRequest())
+      status(response) mustBe NOT_FOUND
+    }
+  }
+
+  "GET Email endpoint" should {
+
+    "return 200(OK) status and deliverable = true if the email address for the given EORI is verified" in {
+      val expectedEmailAddress = Email("some@email.com", deliverable = true)
+
+      when(connector.getEmailAddress(any[String])(any[HeaderCarrier]))
+        .thenReturn(Future.successful(Some(expectedEmailAddress)))
+
+      val response = controller.getEmail(getRequest())
+      status(response) mustBe OK
+      contentAsJson(response) mustBe Json.toJson(expectedEmailAddress)
+
+      verify(connector).getEmailAddress(meq(TestData.eori))(any[HeaderCarrier])
+    }
+
+    "return 200(OK) status and deliverable = false if the email address for the given EORI is not deliverable" in {
+      val expectedEmailAddress = Email("some@email.com", deliverable = false)
+
+      when(connector.getEmailAddress(any[String])(any[HeaderCarrier]))
+        .thenReturn(Future.successful(Some(expectedEmailAddress)))
+
+      val response = controller.getEmail(getRequest())
+      status(response) mustBe OK
+      contentAsJson(response) mustBe Json.toJson(expectedEmailAddress)
+
+      verify(connector).getEmailAddress(meq(TestData.eori))(any[HeaderCarrier])
+    }
+
+    "return 404(NOT_FOUND) status if the email address for the given EORI was not provided or was not verified yet" in {
+      when(connector.getEmailAddress(any[String])(any[HeaderCarrier])).thenReturn(Future.successful(None))
+
+      val response = controller.getEmail(getRequest())
       status(response) mustBe NOT_FOUND
     }
   }
